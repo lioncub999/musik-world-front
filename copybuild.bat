9b74e85a28c5f02e644b1f7ce9f8a30ac9484cc4
leesehwi <leesehwi@minigate.net>
2023-09-05 01:02:24 +0900

    스크립트 수정(용량)

--- copybuild.bat
+++ copybuild.bat
@@ -1,1 +1,2 @@
-xcopy build\*.* ..\musik-world-node\front\musik-world\build\ /e /h /k /y
+@REM xcopy build\*.* ..\musik-world-node\front\musik-world\build\ /e /h /k /y
+echo "삭제 스크립트 넣어야함"
--- copybuild.sh
+++ copybuild.sh
@@ -1,1 +1,3 @@
+rm -r ../musik-world-node/front/musik-world/build
+
 yes|cp -i -f  -a -p -v build ../musik-world-node/front/musik-world/
